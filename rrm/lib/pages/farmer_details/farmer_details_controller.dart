bf04ea50c77341cdb80de08c864055611ec3966c
Apurv Patel <95409482+apurv9112@users.noreply.github.com>
2026-07-20 15:23:02 +0530

    continue 25

--- rrm/lib/pages/farmer_details/farmer_details_controller.dart
+++ rrm/lib/pages/farmer_details/farmer_details_controller.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:get/get.dart';
 import 'package:rrm/services/farmer_services.dart';
@@ -44,16 +43,6 @@ class FarmerDetailsController extends GetxController {
   }
 
   Future<void> shareScreenshot() async {
-    Get.dialog(
-      Center(
-        child: LoadingAnimationWidget.staggeredDotsWave(
-          color: Colors.white,
-          size: 60,
-        ),
-      ),
-      barrierColor: Colors.black45,
-      barrierDismissible: false,
-    );
     try {
       final directory = await getTemporaryDirectory();
       final filePath = '${directory.path}/farmer_screenshot.png';
@@ -65,7 +54,7 @@ class FarmerDetailsController extends GetxController {
       final file = File(filePath);
       await file.writeAsBytes(image);
 
-      await Share.shareXFiles([XFile(filePath)], text: "Farmer Details");
+      await Share.shareXFiles([XFile(filePath)], text: "Report");
     } catch (e) {
       // print("Screenshot error: $e");
     }
--- rrm/lib/pages/kyc/kyc_controller.dart
+++ rrm/lib/pages/kyc/kyc_controller.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:lottie/lottie.dart';
 import 'package:rrm/controller.dart';
 import 'package:rrm/services/kyc_service.dart';
